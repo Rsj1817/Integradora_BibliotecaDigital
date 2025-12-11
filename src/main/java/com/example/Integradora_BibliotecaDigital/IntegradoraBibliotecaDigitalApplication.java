b3951c642b37285dd152fbbb426d0e598b30585e
Rsj1817 <20243ds185@utez.edu.mx>
2025-12-11 01:59:19 -0600

    Primer commit

---
+++ src/main/java/com/example/Integradora_BibliotecaDigital/IntegradoraBibliotecaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Integradora_BibliotecaDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradoraBibliotecaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradoraBibliotecaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Integradora_BibliotecaDigital/IntegradoraBibliotecaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Integradora_BibliotecaDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradoraBibliotecaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
